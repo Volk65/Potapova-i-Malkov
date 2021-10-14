77556fd0e51d2a5041f1726b7c894e9fbeba0dfa
Volk65 <83682418+Volk65@users.noreply.github.com>
2021-10-14 11:39:27 +0300

    Добавьте файлы проекта.

---
+++ Potapova i Malkov/Potapova i Malkov.cpp
@@ -0,0 +1,72 @@
+﻿
+#include <stdio.h>
+
+#include <math.h>
+
+#include <conio.h>
+
+
+
+#define N 10000
+
+
+
+double F(double x)
+
+{
+
+	double f;
+
+	f = sin(x) + cos(x);
+
+	return f;
+
+}
+
+
+
+int main()
+
+{
+
+	double S = 0, x, a, b, h;
+
+	const double Pi = 3.14159;
+
+	a = 0;
+
+	b = Pi;
+
+	//отрезок [a, b] разобьем на N частей
+
+	h = (b - a) / N;
+
+	x = a + h;
+
+	while (x < b)
+
+	{
+
+		S = S + 4 * F(x);
+
+		x = x + h;
+
+		//проверяем не вышло ли значение x за пределы полуинтервала [a, b)
+
+		if (x >= b) break;
+
+		S = S + 2 * F(x);
+
+		x = x + h;
+
+	}
+
+	S = (h / 3) * (S + F(a) + F(b));
+
+	printf("%f", S);
+
+	_getch();
+
+	return 0;
+
+}
